6c70aebdb2411733cb634152167c5c81130ddc42
Navid <Navidjalili34@yahoo.com>
2022-04-26 17:22:54 +0430

    Configure tailwind & include daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,16 @@ module.exports = {
   theme: {
     extend: {},
   },
+  // add daisyUI plugin
   plugins: [require('daisyui')],
+  // daisyUI config
+  daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    themes: ['winter', 'night', 'cmyk'],
+  },
 }
