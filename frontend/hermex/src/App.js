f0976cb1493d017dcb9a920bbd751c2385261b19
Navid <Navidjalili34@yahoo.com>
2022-05-04 16:30:44 +0430

    Add routes to App component

--- frontend/hermex/src/App.js
+++ frontend/hermex/src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 import WelcomePage from './Pages/WelcomePage'
 
 function App() {
   return (
     <>
-      <WelcomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
