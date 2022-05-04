ee812519b6494d8cab01726ede4f663412f9f089
Navid <Navidjalili34@yahoo.com>
2022-05-04 13:06:30 +0430

    Add WelcomePage to pages

---
+++ frontend/hermex/src/Pages/WelcomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+const WelcomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default WelcomePage
