20e79f61143dab715362e2e7a08052df2f9cc0c3
Navid <Navidjalili34@yahoo.com>
2022-05-04 16:21:43 +0430

    Add divider to seperate gallery and news section

---
+++ frontend/hermex/src/Components/Divider.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../Styles/Divider.css'
+
+const Divider = () => {
+  return (
+    <div class="flex flex-col w-full lg:flex-row min-h-full p-4">
+      <div
+        id="gallery"
+        class="grid flex-grow h-96 card bg-base-300 rounded-box place-items-center font-semibold text-4xl text-slate-100"
+      >
+        <Link to="/" className="h-full w-full flex justify-center items-center">
+          Gallery
+        </Link>
+      </div>
+      <div class="divider lg:divider-horizontal"></div>
+      <div
+        id="news"
+        class="grid flex-grow h-96 card bg-base-300 rounded-box place-items-center font-semibold text-4xl text-slate-100"
+      >
+        <Link to="/" className="h-full w-full flex justify-center items-center">
+          News
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Divider
