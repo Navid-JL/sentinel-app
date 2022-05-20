a1b994ad18ca27fe14012d5a1ee172c1241e395c
Navid <Navidjalili34@yahoo.com>
2022-05-20 16:59:56 +0430

    Change class to className to go along with react syntax

--- frontend/hermex/src/Components/Divider.jsx
+++ frontend/hermex/src/Components/Divider.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../Styles/Divider.css'
 
 const Divider = () => {
   return (
-    <div class="flex flex-col w-full lg:flex-row min-h-full p-8">
+    <div className="flex flex-col w-full lg:flex-row min-h-full p-8">
       <div
         id="gallery"
-        class="grid flex-grow h-96 card bg-base-300 rounded-box place-items-center font-semibold text-6xl text-slate-100 transition-all	"
+        className="grid flex-grow h-96 card bg-base-300 rounded-box place-items-center font-semibold text-6xl text-slate-100 transition-all	"
       >
         <Link to="/gallery" className="h-full w-full flex justify-center items-center">
           Gallery
         </Link>
       </div>
-      <div class="divider lg:divider-horizontal"></div>
+      <div className="divider lg:divider-horizontal"></div>
       <div
         id="news"
-        class="grid flex-grow h-96 card bg-base-300 rounded-box place-items-center font-semibold text-6xl text-slate-100 transition-all	"
+        className="grid flex-grow h-96 card bg-base-300 rounded-box place-items-center font-semibold text-6xl text-slate-100 transition-all	"
       >
         <a
           className="h-full w-full flex justify-center items-center"
