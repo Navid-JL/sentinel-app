815be932bf0863d103fea89b26f557c6ef5de7b5
Navid <Navidjalili34@yahoo.com>
2022-05-20 16:58:57 +0430

    Add AboutPage route to routes

--- frontend/hermex/src/App.js
+++ frontend/hermex/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GalleryPage from './Pages/GalleryPage'
 import HomePage from './Pages/HomePage'
+import AboutPage from './Pages/AboutPage'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
           <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </BrowserRouter>
     </>
