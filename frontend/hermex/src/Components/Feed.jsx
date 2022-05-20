07cccfea80f57e11d83dc8f5e68f9a18f9b4a976
Navid <Navidjalili34@yahoo.com>
2022-05-20 17:00:30 +0430

    Add id to feed div

--- frontend/hermex/src/Components/Feed.jsx
+++ frontend/hermex/src/Components/Feed.jsx
@@ -1,12 +1,11 @@
 import Divider from './Divider'
-import '../Styles/Home.css'
 
 const Feed = () => {
   return (
     <>
       <Divider />
-      <div class="divider lg:divider-horizontal"></div>
-      <div className="container mx-auto h-screen">
+      <div className="divider lg:divider-horizontal"></div>
+      <div className="container mx-auto h-screen" id="feed">
         {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 auto-rows-[minmax(0,1fr)]">
         </div> */}
       </div>
