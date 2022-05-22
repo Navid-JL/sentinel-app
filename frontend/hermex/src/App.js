822916ec5c469290287bb0c872017f2bd03852c6
Navid <Navidjalili34@yahoo.com>
2022-05-22 22:26:37 +0430

    Add login, register and error route to App

--- frontend/hermex/src/App.js
+++ frontend/hermex/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GalleryPage from './Pages/GalleryPage'
 import HomePage from './Pages/HomePage'
 import AboutPage from './Pages/AboutPage'
+import RegisterPage from './Pages/RegisterPage'
+import ErrorPage from './Pages/ErrorPage'
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<RegisterPage type="login" />} />
+          <Route path="/register" element={<RegisterPage type="register" />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </>
