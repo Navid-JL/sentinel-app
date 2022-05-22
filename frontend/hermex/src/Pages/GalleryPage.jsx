cf4902ce4eaba72f97d06ccb89dde9b1af2ad20d
Navid <Navidjalili34@yahoo.com>
2022-05-22 22:25:54 +0430

    Remove h-screen class from main div

--- frontend/hermex/src/Pages/GalleryPage.jsx
+++ frontend/hermex/src/Pages/GalleryPage.jsx
@@ -6,7 +6,7 @@ const GalleryPage = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto h-screen">
+      <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 p-4 auto-rows-[minmax(0,1fr)]">
           <ImagePost />
           <ImagePost />
