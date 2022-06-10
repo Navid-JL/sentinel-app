4c6f8d6c26520f77b62e9db397b3cfe5912654fd
Navid <Navidjalili34@yahoo.com>
2022-06-10 12:01:45 +0430

    Create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+      minLength: 12,
+      select: false,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      select: false
+    }
+  },
+  {
+    timestamps: true,
+  }
+)
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashPassword
+    return next()
+  } catch (error) {
+    return next(error)
+  }
+})
+
+userSchema.post('save', async function (next) {
+  try {
+    this.password = undefined
+    this.__v = undefined
+  } catch (error) {
+    next(error)
+  }
+})
+
+userSchema.pre(/^find/, function(next) {
+  this.
+})
+
+module.exports = mongoose.model('User', userSchema)
