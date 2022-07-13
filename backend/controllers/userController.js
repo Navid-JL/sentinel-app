2d9ca2f349fd9e2b7f2f891ed0b6be5d8a103f8b
Navid <Navidjalili34@yahoo.com>
2022-07-13 19:08:25 +0430

    Implement signup, login, logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const User = require('../models/User')
 const { checkName, checkEmail, checkPassword } = require('../utils/validators/validateInputs')
 const encryptPassword = require('../utils/encryptPassword')
 const genJWT = require('../utils/genJWT')
+const bcrypt = require('bcryptjs')
 
 // @desc Register user
 // @route POST /api/v1/users/signup
@@ -19,7 +20,7 @@ exports.registerUser = asyncHandler(async (req, res) => {
   checkEmail(email)
   checkPassword(password)
   // Check if user already exists
-  const userExists = User.findOne({ email }).lean()
+  const userExists = await User.findOne({ email })
   if (userExists) {
     res.status(409)
     throw new Error('User already exists')
@@ -33,12 +34,59 @@ exports.registerUser = asyncHandler(async (req, res) => {
   })
   // If user is successfully registered
   if (user) {
+    // Set loggedIn true in user's session
+    req.session.loggedIn = true
     return res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
-      // Send JWT to the user
-      token: genJWT(user.id),
+    })
+  }
+})
+
+// @desc Authenticate user
+// @route POST /api/users/login
+// @access Public
+exports.loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  // Check if all the required fields are included
+  if (!email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+  // Check whether user's email is standard or not
+  checkEmail(email)
+
+  // Fetch the user from DB
+  const user = await User.findOne({ email }).select('+password')
+
+  // Compare whether user's password match or not
+  if (user && (await bcrypt.compare(password, user.password))) {
+    // Set loggedIn prop in user's session true
+    req.session.loggedIn = true
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+})
+
+// @desc Log user out
+// @route POST /api/users/logout
+// @access Private
+exports.logoutUser = asyncHandler(async (req, res) => {
+  if (req.session) {
+    req.session.destroy((error) => {
+      req.session = null
+      if (error) {
+        throw new Error(error.message)
+      } else {
+        return res.json({ logout: true })
+      }
     })
   }
 })
