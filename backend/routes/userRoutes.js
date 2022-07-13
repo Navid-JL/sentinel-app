e0a86bed6256ceab15061a0eda8a3a3344d1c505
Navid <Navidjalili34@yahoo.com>
2022-07-13 19:04:52 +0430

    Add register, login and logout routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const { registerUser } = require('../controllers/userController')
+const { registerUser, loginUser, logoutUser } = require('../controllers/userController')
 
 const userRouter = express.Router()
 
 // Sign up user
 userRouter.post('/signup', registerUser)
+
+// Log user in
+userRouter.post('/login', loginUser)
+
+// Log user out
+userRouter.post('/logout', logoutUser)
 
 module.exports = userRouter
